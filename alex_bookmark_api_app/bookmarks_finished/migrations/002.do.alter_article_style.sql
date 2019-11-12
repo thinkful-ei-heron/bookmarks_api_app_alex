ce2392c7d9e145e0c87b0ded52ddee5d3d7c92f2
Alex Bannow <rbannal@gmail.com>
2019-11-12 10:54:05 -0500

    initial commit to group repo

---
+++ alex_bookmark_api_app/bookmarks_finished/migrations/002.do.alter_article_style.sql
@@ -0,0 +1,11 @@
+CREATE TyPE article_category AS ENUM (
+  'Listicle',
+  'How-to',
+  'News',
+  'Interview',
+  'Story'
+);
+
+ALTER TABLE blogful_articles
+  ADD COLUMN
+    style article_category;
